9937ec5a6a1098f554f3117d6b483c74b94286a9
vjviolet <gxzj_2015@163.com>
2017-11-03 14:12:49 +0800

    ajax初探

--- 06-Type Ahead/ReadMe.md
+++ 06-Type Ahead/ReadMe.md
@@ -1,6 +1,26 @@
 ###CSS语法
 1. font-weight: 用来表示字体的粗细程度，一些字体只提供normal和bold两种值。`lighter`比从父元素继承来的更细。`bolder`比父元素继承来的值更粗。
 2. box-shadow: inset(表示阴影在里面还是外面) <offset-x> <offset-y> <blur-radius>（表示阴影的厚度） <spread-radius>（这里取正值时，面积扩大，取负值时，面积缩小。） <color>
+3. perspective: 设置从何处查看一个元素的角度。
 
+### fetch: 
+1. Fetch API提供了一个JS接口，用于访问和操纵HTTP管道的部分，例如请求和响应，它还提供了一个全局fetch()方法，该方法提供了一种简单，合乎逻辑的方式来跨网络异步获取资源。
+2. 提供一个参数来指明fetch到的资源路径，然后返回一个promise对象。
+3. promise对象用于一个异步操作的最终完成（或失败）及其结果值的表示。
+4. `fetch(endpoint).then()`这个返回的对象里面有json()方法。
 
+### replace: 
+1. `str.replace(regexp|substr, new Substr|function)`： 该方法是第二个参数来匹配第一个参数。若第二个参数是函数，那么函数返回值将用来替换第一个参数。
 
+### ES6中的扩展语法：
+```js
+//es5中的写法：
+var arr1 = [1, 2, 3];
+var arr2 = [3, 4, 5];
+Array.prototype.push.apply(arr1, arr2); //[1, 2, 3, 3, 4, 5]
+
+//es6中的写法
+const arr1 = [1, 2, 3];
+const arr2 = [3, 4, 5];
+arr1.push(...arr2);
+```
--- 06-Type Ahead/script.js
+++ 06-Type Ahead/script.js
@@ -1,41 +1,53 @@
-window.onload = init;
-function init() {
-	const search = document.querySelector('.search');
-	const suggestions = document.querySelector('.suggestions')
+
 	
 	const endpoint = 'https://gist.githubusercontent.com/soyaine/81399bb2b24ca1bb5313e1985533c640/raw/bdf7df2cbcf70706c4a5e51a7dfb8c933ed78878/TangPoetry.json';
 	const poetries = [];
-	fetch(endpoint)
-		.then(blob => blob.json())
-		.then(data => poetries.push(...data));
+	
 		
-	function findMatches(wordToSearch, poetries) {
-		return poetries.filter(poetry => {
-			const regex = new RegExp(wordToSearch, 'gi');
-			const author = poetry.detail_author.join('');
-	//		console.log(poetry);
-			return poetry.detail_text.match(regex) || poetry.title.match(regex) || author.match(regex);
-		});
-	}
-	
-	function displayMatches() {
-		const matches = findMatches(this.value, poetries);
-		const regex = new RegExp(this.value, 'gi');
-		const html = matches.map(poem => {
-			const text = poem.detail_text.replace(regex, `<span class='hl'>${ this.value }</span>`);
-			const title = poem.title.replace(regex, `<span class= 'hl'>${ this.value }</span>`)
-			return `
-				<li>
-					<span class='poet'>${text}</span>
-					<span class = "title">${title} - ${poem.detail_author[0]} </span>
-				</li>
-			`;
-		}).join(''); //原本是一个数组，要用join将其变成字符串
-//		console.log(html);
-		suggestions.innerHTML = html;
-	}
-	
-	search.addEventListener('change', displayMatches);
-	search.addEventListener('keyup', displayMatches);
-}
 
+
+
+//window.onload = init;
+//
+//function init() {
+//	const poetries = [];
+//	const endpoint = 'https://gist.githubusercontent.com/soyaine/81399bb2b24ca1bb5313e1985533c640/raw/bdf7df2cbcf70706c4a5e51a7dfb8c933ed78878/TangPoetry.json';
+//	fetch(endpoint)
+//		.then(blob => blob.json())
+//		.then(data => poetries.push(...data));
+//	
+//	
+//	function findMatches(wordToSearch, poetries) {
+//		return poetries.filter(poetry => {
+//			const regex = new RegExp(wordToSearch, 'gi');
+//			//建立正则表达式对象
+//			const author = poetry.detail_author.join('');
+//			//找出匹配的对象
+//			return poetry.detail_text.match(regex) || author.match(regex) || poetry.title.match(regex);
+//		})
+//	}
+//	
+//	function displayMatches() {
+//		const matches = findMatches(this.value, poetries);
+//		const regex = new RegExp(this.value, 'gi');
+//		//这里将来设置html文本信息。
+//		const html = matches.map(poem => {
+//			//这里表示用后者替换前者，来讲匹配的文字高亮
+//			const text = poem.detail_text.replace(regex, `<span class = "hl">${ this.value }</span>`);
+//			const title = poem.title.replace(regex, `<span class = "hl">${ this.value }</span>`);
+//			return `
+//				<li>
+//					<span class='poet'>${text}</span>
+//					<span class = "title">${title} - ${poem.detail_author[0]} </span>
+//				</li>
+//			`;
+//		}).join('');
+//		suggestions.innerHTML = html;
+//	}
+//	
+//	const search = document.querySelector('.search');
+//	const suggestions = document.querySelector('.suggestions');
+//	
+//	search.addEventListener('change', displayMatches);
+//	search.addEventListener('keyup', displayMatches);
+//}
