f8c0ad49a371a3eb07ce13671cb946e15e337bd8
vjviolet <gxzj_2015@163.com>
2018-01-15 17:17:36 +0800

    摄像头变化

---
+++ 19-Webcam Fun/script.js
@@ -0,0 +1,7 @@
+const canvas = document.querySelector('.photo');
+const video = document.querySelector('.player');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
+const ctx = canvas.getContext('2d');
+
+//获取摄像头的权限
