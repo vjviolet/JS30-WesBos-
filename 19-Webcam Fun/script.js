14395a4bf32ed44275c55d7b34e5a754db6df24a
vjviolet <gxzj_2015@163.com>
2018-01-16 13:15:07 +0800

    I cannot finish this

--- 19-Webcam Fun/script.js
+++ 19-Webcam Fun/script.js
@@ -5,3 +5,19 @@ const snap = document.querySelector('.snap');
 const ctx = canvas.getContext('2d');
 
 //获取摄像头的权限
+function getVideo() {
+  //这里是获取一个promise对象，是打开摄像头的数据流
+  navigator.mediaDevices.getUserMedia({video: true, audio: false})
+    .then(localMediaStream => {
+      //将blob数据流加进video的src中
+      console.log(localMediaStream);
+      video.srcObject = localMediaStream;
+      //得加上play才能使动画连贯起来，不然动画不会动
+      video.play();
+    })
+    .catch(err => {
+      console.log('OH, Error' + err.name);
+    });
+}
+
+getVideo();
